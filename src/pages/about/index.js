cf509a138fbb318afb29b2411bd89deffc5c30ae
serglaska <onlyjs@protonmail.com>
2019-02-07 16:17:06 +0200

    second fix, after coments

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,55 +2,55 @@ import React from 'react';
 // components
 import PageHeader from '../../components/PageHeader';
 import PageLayout from '../../components/PageLayout';
-import { WrapperForDisplay, RightBg } from './ui';
-//bottom
+//ui
 import  { FooterLink } from '../../components/ui';
 import { Flex, Text } from '../../ui/index';
 // helpers
 import * as H from '../../helpers';
 // hocs
 import { withLocale } from '../../hocs';
+// page about
+import { WrapperForDisplay, RightBg } from './ui';
 //  /////////////////////////////////////////////////////////////////////////////////////////////////
 export const AboutUsWrapper = () => (
   <WrapperForDisplay>
-  <Flex 
-    px='45px'
-    bg='#4A4549'
-    height='100%'
-    alignItems='center'
-    flexDirection='column'
-    justifyContent='center'
-  > 
-
-    <Text
-      py='30px'
-      color='#FBFBFB'
-      fontSize='36px'
-    > About Us
-    </Text>
-
-    <Text
-      color='#FBFBFB'
-      fontSize='18px'
-      maxWidth='500px'
-    >   Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna 
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna 
-      aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-      ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit 
-      esse cillum dolore eu fugiat nulla pariatur. 
-      Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </Text>
-
+    <Flex 
+      px='45px'
+      bg='#4A4549'
+      height='100%'
+      alignItems='center'
+      flexDirection='column'
+      justifyContent='center'
+    > 
+      <Text
+        py='30px'
+        color='#FBFBFB'
+        fontSize='36px'
+      >
+        {H.getLocaleItem(['aboutUs'], props.locale)}
+      </Text>
+      <Text
+        color='#FBFBFB'
+        fontSize='18px'
+        maxWidth='500px'
+      >   
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit 
+        esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        Duis aute irure dolor in reprehenderit in voluptate velit 
+        esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Text>
       <FooterLink
         mt='44px'
         mb='97px'
@@ -61,16 +61,17 @@ export const AboutUsWrapper = () => (
         fontSize='24px'
         textAlign='center'
         borderRadius='5px'
-      > Contact!
+      > 
+        Contact!
       </FooterLink>
     </Flex>
     <RightBg />
   </WrapperForDisplay>
 );
+
 export const AboutPage = props => (
   <PageLayout locale={props.locale}>
-  {/* <PageHeader text={H.getLocaleItem(['aboutUs'], props.locale)} /> */}
-    <AboutUsWrapper />
+    <AboutUsWrapper {...props} />
   </PageLayout>
 );
 
